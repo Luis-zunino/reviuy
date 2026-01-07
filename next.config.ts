117f491f17d86a7c33fdaa3fea56b260c543f642
Luis-zunino <zuninoluis01@gmail.com>
2026-01-07 19:03:50 -0300

    feat: update security headers by strengthening Referrer-Policy and expanding Content-Security-Policy with new directives and image sources.

--- next.config.ts
+++ next.config.ts
@@ -41,12 +41,41 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            value: 'strict-origin-when-cross-origin',
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://placehold.co https://firebasestorage.googleapis.com https://lh3.googleusercontent.com; font-src 'self' data:; connect-src 'self' https://*.supabase.co https://firebasestorage.googleapis.com https://vitals.vercel-insights.com https://nominatim.openstreetmap.org;",
-          },
+            value: `
+                    default-src 'self';
+                    script-src
+                    'self'
+                    'unsafe-inline'
+                    'unsafe-eval'
+                    https://apis.google.com
+                    https://va.vercel-scripts.com;
+                    style-src
+                    'self'
+                    'unsafe-inline';
+                    img-src
+                    'self'
+                    data:
+                    blob:
+                    https://placehold.co
+                    https://firebasestorage.googleapis.com
+                    https://lh3.googleusercontent.com
+                    https://*.tile.openstreetmap.org;
+                    font-src 'self' data:;
+                    connect-src
+                    'self'
+                    https://*.supabase.co
+                    https://firebasestorage.googleapis.com
+                    https://vitals.vercel-insights.com
+                    https://nominatim.openstreetmap.org;
+                    object-src 'none';
+                    base-uri 'self';
+                    frame-ancestors 'none';
+                    `.replace(/\s{2,}/g, ' ').trim(),
+          }
         ],
       },
       {
@@ -67,7 +96,7 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-       {
+      {
         source: '/(.*)',
         headers: [
           {
