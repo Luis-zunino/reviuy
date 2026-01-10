6e26ec05190dab544fd6b7a77b26e02733497647
Luis-zunino <zuninoluis01@gmail.com>
2026-01-09 22:29:51 -0300

    feat: delete external page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PagesUrls } from '@/enums';
-import { redirect } from 'next/navigation';
-
-const RootPage = () => {
-  redirect(PagesUrls.HOME);
-  return null;
-};
-
-export default RootPage;
