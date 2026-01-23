33ae3edf4a08ad90e79467a775261688db012696
Luis-zunino <zuninoluis01@gmail.com>
2026-01-22 23:26:53 -0300

    feat: format file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,7 +18,8 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
-      }, {
+      },
+      {
         src: '/icon.512.png',
         sizes: '512x512',
         type: 'image/png',
@@ -28,6 +29,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/favicon.svg',
         type: 'image/svg+xml',
         purpose: 'any',
-      },]
+      },
+    ],
   };
 }
