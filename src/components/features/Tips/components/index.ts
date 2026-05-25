83b08d8b4a4882cae95107d1a167904fa1f2684e
Luis Zunino <zuninoluis01@gmail.com>
2026-05-25 00:21:35 -0300

    refactor: remove unused type definitions and components

--- src/components/features/Tips/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { TipsSidebar } from './TipsSidebar';
--- src/modules/_templates/domain/repository.template.ts
+++
@@ -1,7 +0,0 @@
-export interface ExampleEntity {
-  id: string;
-}
-
-export interface ExampleRepository {
-  findById(id: string): Promise<ExampleEntity | null>;
-}
