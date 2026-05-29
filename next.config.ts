a1eaf12b2de6f1bbe36bf989b71d6b5967060ceb
Luis Zunino <zuninoluis01@gmail.com>
2026-05-29 10:14:47 -0300

    feat: update Sentry configuration for improved error tracking and logging

--- next.config.ts
+++ next.config.ts
@@ -111,9 +111,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "reviuy",
+  org: 'reviuy-2a',
 
-  project: "javascript-nextjs",
+  project: 'sentry-amber-basket',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
@@ -128,7 +128,7 @@ export default withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: '/monitoring',
 
   webpack: {
     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
